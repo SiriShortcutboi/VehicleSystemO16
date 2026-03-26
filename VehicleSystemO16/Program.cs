a1731321eaea6c19336f4bc6aee47e3a018bee1a
SiriShortcutboi <58081965+SiriShortcutboi@users.noreply.github.com>
2026-03-26 04:05:21 -0600

    good stop for tonight

---
+++ VehicleSystemO16/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Truck truck = new Truck();
+        truck.StartEngine(); // Output: Truck engine started
+        truck.LoadCargo(); // Output: Truck cargo loaded
+
+        Car Car = new Car();
+        Car.StartEngine();
+        car.PlayRadio();
+
+        Motorcycle motorcycle = new Motorcycle();
+        motorcycle.StartEngine(); 
+        motorcycle.PopWheelie();
+	}
+}
---
+++ VehicleSystemO16/Vehicle.cs
@@ -0,0 +1,10 @@
+namespace EnginePowers;
+
+public class Vehicle
+{
+    public static string StartEngine()
+    {
+        string ignitionkey = "Vroom Rumble Rumble Vroom";
+        return ignitionkey;
+    }
+}
